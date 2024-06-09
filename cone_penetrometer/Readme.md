6695db0d89a4b9a59e92b862e129eb2c213855a3
bharatind12 <bharatkhanvilkar12@gmail.com>
2024-06-09 23:19:20 +0530

    testing container

---
+++ cone_penetrometer/Readme.md
@@ -0,0 +1,17 @@
+# Static Webpage extension
+
+This uses python to serve a static HTML page using Vue and Vuetify as an extension. The static page in this example talks to a simple python backend.
+
+to build:
+
+Enable qemu static support with a docker
+
+```
+docker buildx create --name multiarch --driver docker-container --use
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+```
+
+Then build it:
+
+`docker buildx build --platform linux/amd64,linux/arm/v7 . -t varunn30/multiarch:latest --output type=registry
+`
---
+++ cone_penetrometer/app/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request, send_from_directory
+import serial
+import time
+import os
+
+app = Flask(__name__, static_folder='templates')
+
+# Configure the serial port
+ser = serial.Serial('/dev/ttyS0', 9600)  # Replace '/dev/ttyS0' with the appropriate serial port for your Arduino
+time.sleep(2)  # Wait for the serial connection to initialize
+
+# Function to send command to Arduino and read response
+def send_command(command):
+    ser.write(command.encode())
+    time.sleep(0.1)  # Wait for Arduino to process the command
+    response = ser.readline().decode().strip()
+    return response
+
+@app.route('/send_command', methods=['POST'])
+def handle_send_command():
+    data = request.get_json()
+    command = data.get('command')
+    response = send_command(command)
+    return jsonify({'response': response})
+
+@app.route('/', methods=['GET'])
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        ser.close()
