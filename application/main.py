72b5b252fd11c8d62d324f8aa21e5db3c8a0e7c1
bharatind12 <bharatkhanvilkar12@gmail.com>
2024-06-13 22:21:00 +0530

    running and stable version of docker error in main.py

--- application/main.py
+++ application/main.py
@@ -1,19 +1,36 @@
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 import os
-from bson.objectid import ObjectId
 import serial
 import time
 
 app = Flask(__name__)
 
-# MongoDB configuration
-client = MongoClient('mongodb://localhost:27017/')
-db = client['cone_penetrometer']
-collection = db['sessions']
+# SQLite configuration
+DATABASE_URL = 'sqlite:///sqlite.db'
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Define SQLAlchemy ORM model
+class SessionData(Base):
+    __tablename__ = 'sessions'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sessionName = Column(String)
+    threshold = Column(Integer)
+    numberOfPenetrations = Column(Integer, default=0)
+    numberOfTurns = Column(Integer, default=0)
+    sensorValue = Column(Integer, default=0)
+    status = Column(String, default='NO-GO')
+
+Base.metadata.create_all(engine)
 
 # Serial configuration
-arduino = serial.Serial('/dev/ttyS0', 9600)  # Replace 'COM3' with your Arduino's COM port
+arduino = serial.Serial('/dev/ttyS0', 9600)  # Replace with your Arduino serial configuration
 time.sleep(2)  # Wait for the serial connection to initialize
 
 def send_command_to_arduino(command):
@@ -21,6 +38,12 @@ def send_command_to_arduino(command):
     time.sleep(1)  # Give Arduino time to process the command
     response = arduino.readline().decode().strip()
     return response
+
+app.mount("/", StaticFiles(directory="static",html = True), name="static")
+
+@app.get("/", response_class=FileResponse)
+async def root() -> Any:
+        return "index.html"
 
 @app.route('/')
 def home():
@@ -31,64 +54,64 @@ def submit_form():
     data = request.json
     session_name = data.get('sessionName')
     threshold = int(data.get('threshold'))
-    session_data = {
-        'sessionName': session_name,
-        'threshold': threshold,
-        'numberOfPenetrations': 0,
-        'numberOfTurns': 0,
-        'sensorValue': 0,
-        'status': 'NO-GO'
-    }
-    result = collection.insert_one(session_data)
-    return jsonify({"message": "Form submitted successfully", "session_id": str(result.inserted_id)})
+    
+    # Create new session in SQLite database
+    session = SessionData(sessionName=session_name, threshold=threshold)
+    session.add(session)
+    session.commit()
+    
+    return jsonify({"message": "Form submitted successfully", "session_id": session.id})
 
 @app.route('/go_down', methods=['POST'])
 def go_down():
     data = request.json
-    session_id = ObjectId(data.get('session_id'))
-    session = collection.find_one({'_id': session_id})
+    session_id = int(data.get('session_id'))
     
-    numberOfTurns = session['numberOfTurns'] + 1
-    sensorValue = int(send_command_to_arduino("GO_DOWN\n"))  # Get sensor value from Arduino
-    status = 'NO-GO'
+    # Retrieve session from SQLite database
+    session = Session.query(SessionData).filter_by(id=session_id).first()
     
-    if sensorValue >= session['threshold']:
-        numberOfPenetrations = session['numberOfPenetrations'] + 1
-        numberOfTurns = 0
-        status = 'GO-GO'
-    else:
-        numberOfPenetrations = session['numberOfPenetrations']
-
-    collection.update_one({'_id': session_id}, {"$set": {
-        'numberOfTurns': numberOfTurns,
-        'sensorValue': sensorValue,
-        'status': status,
-        'numberOfPenetrations': numberOfPenetrations
-    }})
+    session.numberOfTurns += 1
+    session.sensorValue = int(send_command_to_arduino("GO_DOWN\n"))  # Get sensor value from Arduino
+    session.status = 'NO-GO'
     
-    updated_session = collection.find_one({'_id': session_id})
-    return jsonify(updated_session)
+    if session.sensorValue >= session.threshold:
+        session.numberOfPenetrations += 1
+        session.numberOfTurns = 0
+        session.status = 'GO-GO'
+    
+    Session.commit()
+    
+    return jsonify({
+        'session_id': session.id,
+        'numberOfPenetrations': session.numberOfPenetrations,
+        'numberOfTurns': session.numberOfTurns,
+        'sensorValue': session.sensorValue,
+        'status': session.status
+    })
 
 @app.route('/retract', methods=['POST'])
 def retract():
     data = request.json
-    session_id = ObjectId(data.get('session_id'))
-    session = collection.find_one({'_id': session_id})
+    session_id = int(data.get('session_id'))
     
-    numberOfPenetrations = session['numberOfPenetrations'] + 1
-    numberOfTurns = 0
-    status = 'NO-GO'
+    # Retrieve session from SQLite database
+    session = Session.query(SessionData).filter_by(id=session_id).first()
     
-    retract_status = send_command_to_arduino("RETRACT\n")
+    session.numberOfPenetrations += 1
+    session.numberOfTurns = 0
+    session.status = 'NO-GO'
     
-    collection.update_one({'_id': session_id}, {"$set": {
-        'numberOfPenetrations': numberOfPenetrations,
-        'numberOfTurns': numberOfTurns,
-        'status': status
-    }})
+    send_command_to_arduino("RETRACT\n")
     
-    updated_session = collection.find_one({'_id': session_id})
-    return jsonify(updated_session)
+    Session.commit()
+    
+    return jsonify({
+        'session_id': session.id,
+        'numberOfPenetrations': session.numberOfPenetrations,
+        'numberOfTurns': session.numberOfTurns,
+        'sensorValue': session.sensorValue,
+        'status': session.status
+    })
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
